59ed4f3873ac31af07b74dd4f3636b0dfc40d2e4
Marten <martinrages@gmail.com>
2025-01-25 21:36:30 +0100

    Formatting

--- Engine/Program.cs
+++ Engine/Program.cs
@@ -19,7 +19,7 @@ window.SetRenderCallback(new GraphicsCallback((a, w) =>
     a.SetClearColor(new Vector4(0.2f, 0.4f, 0.6f, 1.0f));
     a.SetViewport(0,0,w.Width,w.Height);
     
-    mesh!.Draw();
+    mesh.Draw();
     
     w.SwapBuffers();
 }));
@@ -28,45 +28,46 @@ window.SetUpdateCallback((w) =>
 {
     if (w.Input.IsKeyDown(Key.A))
     {
-        mesh!.Transform.Position += new Vector3(-0.1f, 0.0f, 0.0f) * (float)w.DeltaTime;
+        mesh.Transform.Position += new Vector3(-0.1f, 0.0f, 0.0f) * (float)w.DeltaTime;
     }
 
     if (w.Input.IsKeyDown(Key.D))
     {
-        mesh!.Transform.Position += new Vector3(0.1f, 0.0f, 0.0f) * (float)w.DeltaTime;
+        mesh.Transform.Position += new Vector3(0.1f, 0.0f, 0.0f) * (float)w.DeltaTime;
     }
     
     if (w.Input.IsKeyDown(Key.W))
     {
-        mesh!.Transform.Position += new Vector3(0.0f, 0.1f, 0.0f) * (float)w.DeltaTime;
+        mesh.Transform.Position += new Vector3(0.0f, 0.1f, 0.0f) * (float)w.DeltaTime;
     }
 
     if (w.Input.IsKeyDown(Key.S))
     {
-        mesh!.Transform.Position += new Vector3(0.0f, -0.1f, 0.0f) * (float)w.DeltaTime;
+        mesh.Transform.Position += new Vector3(0.0f, -0.1f, 0.0f) * (float)w.DeltaTime;
     }
 });
 
 window.SetLoadCallback((w) =>
 {
     var shader = new Shader(w.GraphicsApi, File.ReadAllText("Shaders/Basic.vert"), File.ReadAllText("Shaders/Basic.frag"));
-    mesh = new Mesh(w.GraphicsApi, shader);
     
-    // Square, Vec3, Vec2 (uv)
-    mesh.Vertices = new float[]
+    mesh = new Mesh(w.GraphicsApi, shader)
     {
-        -0.5f, -0.5f, 0.0f, 0.0f, 0.0f,
-        0.5f, -0.5f, 0.0f, 1.0f, 0.0f,
-        0.5f, 0.5f, 0.0f, 1.0f, 1.0f,
-        -0.5f, 0.5f, 0.0f, 0.0f, 1.0f
+        // Square, Vec3, Vec2 (uv)
+        Vertices = new float[]
+        {
+            -0.5f, -0.5f, 0.0f, 0.0f, 0.0f,
+            0.5f, -0.5f, 0.0f, 1.0f, 0.0f,
+            0.5f, 0.5f, 0.0f, 1.0f, 1.0f,
+            -0.5f, 0.5f, 0.0f, 0.0f, 1.0f
+        },
+        Indices = new uint[]
+        {
+            0, 1, 2,
+            2, 3, 0
+        }
     };
-    
-    mesh.Indices = new uint[]
-    {
-        0, 1, 2,
-        2, 3, 0
-    };
-    
+
     mesh.Init();
     
     var texture = new Texture(w.GraphicsApi, "Textures/example.png");
